04bf3618132053bf8642b5b9b918f0aa31aff92b
JohnF1103 <87583884+JohnF1103@users.noreply.github.com>
2023-12-19 02:16:23 -0500

    move

---
+++ New_Foreflight/Views/ContentView.swift
@@ -0,0 +1,30 @@
+
+//
+//  ContentView.swift
+//  New_Foreflight
+//
+//  Created by John Foster on 12/15/23.
+//
+import SwiftUI
+import Foundation
+import MapKit
+
+struct ContentView: View {
+
+    
+
+    
+    
+    var body: some View {
+          Map {
+              
+          }.mapStyle(.hybrid(elevation: .realistic))
+        
+        
+      
+      }
+    
+    
+}
+
+
