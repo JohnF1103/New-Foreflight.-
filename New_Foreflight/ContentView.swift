9c6da24a6e9eec7d0d8855849055672956effc7f
JohnF1103 <87583884+JohnF1103@users.noreply.github.com>
2023-12-19 23:58:55 -0500

    airport annotations

--- New_Foreflight/ContentView.swift
+++ New_Foreflight/ContentView.swift
@@ -1,4 +1,3 @@
-
 //
 //  ContentView.swift
 //  New_Foreflight
@@ -6,30 +5,55 @@
 //  Created by John Foster on 12/15/23.
 //
 import SwiftUI
-import Foundation
 import MapKit
 
-struct RainOverlay: View {
-    var body: some View {
-        // Customize this view to display rain data (e.g., raindrops, layers, etc.)
-        Circle()
-            .foregroundColor(.blue)
-            .opacity(0.5)
-            .frame(width: 20, height: 20)
-    }
+
+
+struct Location: Identifiable, Codable, Equatable {
+    let id: UUID
+    let AirportCode : String
+    let latitude: Double
+    let longitude: Double
+}
+
+
+func readFile() -> [Location] {
+    var locs: [Location] = []
+
+    let filename = "Airports.txt"
+        var str1: String
+        var myCounter: Int
+        do {
+            let contents = try String(contentsOfFile: filename)
+            let lines = contents.split(separator:"\n")
+            myCounter = lines.count
+            str1 = String(myCounter)
+            } catch {
+                print(error.localizedDescription)
+            }
+    
+
+    return locs
 }
 
 struct ContentView: View {
+    @State private var locations: [Location] = readFile()
+
     var body: some View {
         Map {
-            // Add your map annotations or other map features here if needed
+            ForEach(locations) { location in
+                Annotation(location.AirportCode, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
+                    Button(action: { print("Clicked on \(location.AirportCode)") }, label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.teal)
+                            Text("✈️")
+                                .padding(5)
+                        }
+                    })
+                }
+            }
         }
         .mapStyle(.hybrid(elevation: .realistic))
-        .overlay(
-            RainOverlay()
-                // Adjust the overlay position as needed
-                .offset(x: 50, y: -50)
-        )
     }
 }
-
