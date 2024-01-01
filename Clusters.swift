e0bbcb82a35d2ff27b2548dbcc0ae4e6237b01a0
JohnF1103 <87583884+JohnF1103@users.noreply.github.com>
2024-01-01 01:58:44 -0500

    cluster annotations, arch for MKmapview

---
+++ Clusters.swift
@@ -0,0 +1,8 @@
+//
+//  Clusters.swift
+//  New_Foreflight
+//
+//  Created by John Foster on 12/31/23.
+//
+
+import Foundation
---
+++ MapView .swift
@@ -0,0 +1,8 @@
+//
+//  MapView .swift
+//  New_Foreflight
+//
+//  Created by John Foster on 12/31/23.
+//
+
+import Foundation
