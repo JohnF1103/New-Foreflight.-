8ac968b0a6ff5dc023a3ee7f947536a33746ba4c
mkwiiallpro <mkwiiallpro@gmail.com>
2025-02-04 17:46:26 -0500

    began work on a runway list

--- New_Foreflight/RunwaysView.swift
+++ New_Foreflight/RunwaysView.swift
@@ -6,13 +6,23 @@
 //
 
 import SwiftUI
-
+struct Runway : Identifiable{
+    var id = UUID()
+    var heading : String
+}
 struct RunwaysView: View {
+    let curr_ap: Airport
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            Text("04L-22R")
+            Text("04R-22L")
+            Text("11-29")
+        }
     }
 }
 
 #Preview {
-    RunwaysView()
+    /*RunwaysView(curr_ap: Airport(
+        id: UUID(),AirportCode: "KEWR", latitude: 40.69222222222222, longitdue: -74.16861111111112) )*/
 }
